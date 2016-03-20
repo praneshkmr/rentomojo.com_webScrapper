7d1855cd2656aa82cc10efc1658ca0a27590957b
Pranesh Kumar <praneshkmr@gmail.com>
2016-03-20 23:38:47 +0530

    Basic Scrapper with Link Extractor

---
+++ app.js
@@ -0,0 +1,50 @@
+var request = require('request');
+
+var jsdom = require("jsdom");
+var window = jsdom.jsdom().defaultView;
+
+//var $=require('jquery');
+
+var options = {
+  url: 'https://www.rentomojo.com/?_escaped_fragment_',
+  headers: {
+    'User-Agent': 'Node.js Scraper'
+  }
+};
+
+var fs = require('fs');
+var body = fs.readFileSync("./body.txt", "utf-8");
+findHyperlinks(body, function(err, links) {
+  if (err) {
+    console.log(err);
+  }
+  else {
+    console.log(links);
+  }
+});
+
+//$("<h1>test passes</h1>").appendTo("body");
+//console.log($("body").html());
+
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    //$(body).appendTo("body");
+    //console.log($("body").html());
+
+  }
+}
+
+function findHyperlinks(html, callback){
+  jsdom.jQueryify(window, "./jquery.js", function () {
+    var $ = window.$;
+    var anchors = $(html).find("a");
+    var links = [];
+    anchors.each(function( index ) {
+      var link = $( this ).attr("href");
+      links.push(link);
+    });
+    callback(null,links);
+  });
+}
+
+//request(options, callback);
