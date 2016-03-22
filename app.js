87955e9f4abcc8450554728e3ae9153673176bcf
Pranesh Kumar <praneshkmr@gmail.com>
2016-03-23 01:41:46 +0530

    Changed command line display options to be more meaningful

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var scrapperSync = require('./app/services/scrapperSync');
 var scrapperAsync = require('./app/services/scrapperAsync');
 
 console.log("Enter which mode to scrap website");
-console.log("1. Sync");
-console.log("2. Async");
+console.log("1. Without Async Library");
+console.log("2. With Async Library");
 
 var answer = readlineSync.questionInt('Type a number : ');
 if(answer == 1){
